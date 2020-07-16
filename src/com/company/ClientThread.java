ed784ff7327d2cc1797c55f34f92e215ac8ca080
1TaNaTa1 <Stoyan.99iliev@gmail.com>
2020-07-16 23:28:52 +0300

    Created the login method and the printClientServicesInfo method

--- src/com/company/ClientThread.java
+++ src/com/company/ClientThread.java
@@ -1,19 +1,146 @@
 package com.company;
 
+import com.company.models.PhoneNumber;
+import com.company.models.PhoneNumberService;
+import com.company.repositories.ClientRepository;
+
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
 
 public class ClientThread implements Runnable {
     private Socket clientSocket;
     private Connection connection;
+    private long clientId;
+
+    private Scanner scanner;
+    private PrintStream printout;
+
+    private ClientRepository repository;
 
     public ClientThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
         connection = DatabaseConnection.createConnection();
+        repository = new ClientRepository(connection);
     }
 
     @Override
     public void run() {
+        try {
+            scanner = new Scanner(clientSocket.getInputStream());
+            printout = new PrintStream(clientSocket.getOutputStream());
 
+            clientId = getLoginId();
+            if (clientId == -1) {
+                printout.println("Goodbye");
+                scanner.close();
+                printout.close();
+                return;
+            } else if (clientId == 0) {
+                printout.println("We are sorry, you are not allowed access!");
+                scanner.close();
+                printout.close();
+                return;
+            }
+
+            while (true) {
+                printMenu();
+                String input = scanner.nextLine().toLowerCase();
+
+                if (input.equals("quit")) {
+                    printout.println("Goodbye");
+                    scanner.close();
+                    printout.close();
+                    break;
+                }
+
+                switch (input) {
+                    case "1":
+                        printClientServicesInfo();
+                        break;
+                    case "2":
+//                        addNewClient();
+                        break;
+                    default:
+                        printout.println("Enter valid command.");
+
+                }
+            }
+
+            scanner.close();
+            printout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void printClientServicesInfo() {
+        try {
+            List<PhoneNumber> phoneNumbers = repository.getClientPhoneNumber(clientId);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (PhoneNumber phoneNumber : phoneNumbers) {
+                List<PhoneNumberService> services = repository.
+                        getServicePhoneNumberByPhoneId(phoneNumber.getId());
+
+                for (PhoneNumberService service : services) {
+                    stringBuilder
+                            .append(service.getRemainingValue())
+                            .append(" ")
+                            .append(service.getName())
+                            .append(" until ")
+                            .append(service.getDeactivationDate())
+                            .append(System.lineSeparator());
+                }
+            }
+
+            printout.println(stringBuilder);
+        } catch (SQLException e) {
+            printout.println("Something went wrong try again");
+        }
+    }
+
+    private void printMenu() {
+        String[] menu = {
+                "Enter:",
+                "1 To view information about your active services and the remaining Minutes, MBs or SMSes.",
+                "2 To view the next payment date for the services you have not paid",
+                "Quit For exit from the program."};
+
+        printout.println(String.join(System.lineSeparator(), menu));
+        Utils.sendStopSignal(printout);
+    }
+
+    private long getLoginId() {
+        long isLogged = -1;
+        for (int i = 0; i < 3; i++) {
+            printout.println("Enter username or enter quit to exit:");
+            Utils.sendStopSignal(printout);
+            String username = scanner.nextLine();
+
+            if (username.toLowerCase().equals("quit")) {
+                return isLogged;
+            }
+
+            printout.println("Enter password:");
+            Utils.sendStopSignal(printout);
+            String password = scanner.nextLine();
+
+            try {
+                isLogged = repository.isLogged(username, password);
+                return isLogged;
+            } catch (SQLException e) {
+                printout.println("Error occurred. Please try again!");
+                if (i == 2) {
+                    Utils.sendStopSignal(printout);
+                }
+            }
+        }
+        return isLogged;
     }
 }
